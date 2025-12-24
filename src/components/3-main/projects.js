e56ea7b75a559a1b4cecda20b11ad1f8235095a0
Anas-hessein <anas.hessen15@gmail.com>
2025-12-24 18:12:37 +0200

    final

--- src/components/3-main/projects.js
+++ src/components/3-main/projects.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     projectTitle: "swiril ice",
     category: "css",
-    imgPath: "../ice.png",
+    imgPath: "/ice.png",
     description: "This is my personal website to share with you some informations about me, so dig in and read more about me.",
     plyblePath: "https://anas-hessein.github.io/swirilice/",
     gitPath: "https://github.com/Anas-hessein/swirilice",
@@ -11,7 +11,7 @@ export const projects = [
   {
     projectTitle: "One Pan Disaster",
     category: "css",
-    imgPath: "../one.png",
+    imgPath: "/one.png",
     description: "Small website creates foolish random recipes with a click, most will sound barely edible.",
     plyblePath: "https://anas-hessein.github.io/One-Pan-Disaster/",
     gitPath: "https://github.com/Anas-hessein/One-Pan-Disaster",
@@ -21,7 +21,7 @@ export const projects = [
   {
     projectTitle: "Pop Coin",
     category: "js",
-    imgPath: "../cat.png",
+    imgPath: "/cat.png",
     description: "Popcoin is popcat web game with coins and new features like, counting clicks ,4 skins for the popcat and will be coming soon a feature of changing the theme of the page by the coins",
     plyblePath: "https://anas-hessein.github.io/PopCoin/",
     gitPath: "https://github.com/Anas-hessein/PopCoin",
@@ -31,7 +31,7 @@ export const projects = [
   {
     projectTitle: "Recipe API",
     category: "js",
-    imgPath: "../end.png",
+    imgPath: "/end.png",
     description: "My first API with recipe endpoints, JWT Authentication and Swagger UI.",
     plyblePath: "https://endpointer-v2-anas.vercel.app/",
     gitPath: "https://github.com/Anas-hessein/endpointer-v2--anas",
@@ -41,7 +41,7 @@ export const projects = [
   {
     projectTitle: "GenO",
     category: "js",
-    imgPath: "../gen.png",
+    imgPath: "/gen.png",
     description: "My second Js project. It's a password generator with click copy feature.",
     plyblePath: "https://anas-hessein.github.io/GenO/",
     gitPath: "https://github.com/Anas-hessein/GenO",
@@ -51,7 +51,7 @@ export const projects = [
   {
     projectTitle: "solo.js",
     category: "js",
-    imgPath: "../solo.png",
+    imgPath: "/solo.png",
     description: "This is my first web app js. its a counting app for counting the houses and guest.",
     plyblePath: "https://anas-hessein.github.io/solo.js/",
     gitPath: "https://github.com/Anas-hessein/solo.js",
@@ -61,7 +61,7 @@ export const projects = [
   {
     projectTitle: "my town",
     category: "css",
-    imgPath: "../town.png",
+    imgPath: "/town.png",
     description: "A website that creates opportunities for cultural exchange with Hack Clubbers.",
     plyblePath: "https://anas-hessein.github.io/my-town/",
     gitPath: "https://github.com/Anas-hessein/my-town",
@@ -71,7 +71,7 @@ export const projects = [
   {
     projectTitle: "porto gurb",
     category: "other",
-    imgPath: "../port.png",
+    imgPath: "/port.png",
     description: "My first portofolio i made with tailwind css. it hase a footer, sidebar, navbar, darkmode toggle button, redirecting tags, and word gym that changes color.",
     plyblePath: "https://anas-hessein.github.io/porto-gurb/",
     gitPath: "https://github.com/Anas-hessein/porto-gurb",
@@ -81,7 +81,7 @@ export const projects = [
   {
     projectTitle: "goose",
     category: "css",
-    imgPath: "../piz.png",
+    imgPath: "/piz.png",
     description: "my first html, css app.",
     plyblePath: "https://anas-hessein.github.io/PIZza/",
     gitPath: "https://github.com/Anas-hessein/PIZza",
@@ -91,7 +91,7 @@ export const projects = [
   {
     projectTitle: "Solder Anas",
     category: "other",
-    imgPath: "../sus.png",
+    imgPath: "/sus.png",
     description: "A light-sensitive owl PCB with glowing LED eyes (photoresistor-controlled) and a button that makes its tiny DC motor wings buzz to life, like its trying to fly off your desk.",
     plyblePath: "https://github.com/Anas-hessein/Solder_Anas/blob/main/drill.rar",
     gitPath: "https://github.com/Anas-hessein/Solder_Anas",
@@ -101,7 +101,7 @@ export const projects = [
   {
     projectTitle: "tabyslap extension",
     category: "js",
-    imgPath: "../tab.png",
+    imgPath: "/tab.png",
     description: "Slaps you with a roast when you open too many tabs.",
     plyblePath: "https://github.com/Anas-hessein/tabyslap-extension/releases/tag/v2.0",
     gitPath: "https://github.com/Anas-hessein/tabyslap-extension",
@@ -111,7 +111,7 @@ export const projects = [
   {
     projectTitle: "BiteRight",
     category: "js",
-    imgPath: "../bit.png",
+    imgPath: "/bit.png",
     description: "BiteRight extensions show you what type of food you should eat to have a healthier life style.",
     plyblePath: "https://github.com/Anas-hessein/BiteRight/releases/tag/hi",
     gitPath: "https://github.com/Anas-hessein/BiteRight",
@@ -121,7 +121,7 @@ export const projects = [
   {
     projectTitle: "BiteRight v2",
     category: "js",
-    imgPath: "../bit2.png",
+    imgPath: "/bit2.png",
     description: "Redirect junk-food browsing to healthier, Suggest healther food sites. its suggest to you a healthy website with big flag. BiteRight extensions show you what type of food you should eat to have a healthier life style. It has a feature of limiting taps for not overcoming with food orders.",
     plyblePath: "https://github.com/Anas-hessein/BiteRight-v2/releases/tag/ads",
     gitPath: "https://github.com/Anas-hessein/BiteRight-v2",
@@ -131,7 +131,7 @@ export const projects = [
   {
     projectTitle: "frontend playground",
     category: "other",
-    imgPath: "../fort.png",
+    imgPath: "/fort.png",
     description: "A sandbox you can try in it your html, css, tailwind css ,and JS. upcoming React and Vue and will be added a ready made templet for react to use.",
     plyblePath: "https://github.com/Anas-hessein/frontend-playground-/blob/main/FrontendPlayground-1.0.0.vsix",
     gitPath: "https://github.com/Anas-hessein/frontend-playground-",
@@ -141,7 +141,7 @@ export const projects = [
   {
     projectTitle: "React portfolio",
     category: "react",
-    imgPath: "../rct.png",
+    imgPath: "/rct.png",
     description: "React portfolio. This is my first react web app. it has some errors and i will fix it in the future. it's the biggest project I have ever made.",
     plyblePath: "https://anas-hessein.github.io/swirilice/",
     gitPath: "https://github.com/Anas-hessein/swirilice",
